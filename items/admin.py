3a81bd1c423ca5833f7b14df5ab7db420c6dd799
yusuf963 <mohammad963yusuf@gmail.com>
2022-05-21 16:00:28 +0100

    add filter, search and display functions to admin site

--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Item, Category
 
-admin.site.register(Item)
-admin.site.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'item_state', 'owner', 'rate')
+    list_display_links = ('name', 'category', 'item_state', 'owner', 'rate')
+    list_filter = ('category','name', 'item_state', 'owner')
+    search_fields = ('name', 'category__name')
+    list_per_page = 25
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'color_tag')
+    list_display_links = ('name', 'description', 'color_tag')
+    list_filter = ('name', 'description', 'color_tag')
+    search_fields = ('name', 'description', 'color_tag')
+    list_per_page = 25
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
